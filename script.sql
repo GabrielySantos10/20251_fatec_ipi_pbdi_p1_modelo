54c537ef42a6372535f51dbd54f5d3ac99cbbe5a
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-04-29 11:20:59 -0300

    feat(p1): encontra aprovados com pais PhDs

--- script.sql
+++ script.sql
@@ -6,14 +6,39 @@ CREATE TABLE alunos(
 	MOTHER_EDU INT,
 	FATHER_EDU INT,
 	GRADE INT,
-	PREP_STUDY INT,
+	PREP_STUDY INT,	
 	PREP_EXAM INT,
 	SALARY INT
 );
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
-
+-- DO
+-- $$
+-- DECLARE
+-- cur_id_estudante REFCURSOR;
+-- c_estudante RECORD;
+-- alunos_aprovados INTEGER := 0;
+-- BEGIN
+--   OPEN cur_id_estudante FOR
+--     SELECT 
+-- 	STUDANTID,
+-- 	GRADE, 
+-- 	FATHER_EDU, 
+-- 	MOTHER_EDU
+--     FROM alunos
+-- 	WHERE FATHER_EDU = 6 OR MOTHER_EDU = 6
+--     AND GRADE >= 5;  
+--  LOOP
+--    FETCH cur_id_estudante INTO c_estudante;
+--    EXIT WHEN NOT FOUND;
+--    alunos_aprovados := alunos_aprovados + 1;
+--  END LOOP;
+   
+--  RAISE NOTICE 'Total de alunos aprovados é: %', alunos_aprovados;
+--  CLOSE cur_id_estudante;
+-- END 
+-- $$;
 
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
