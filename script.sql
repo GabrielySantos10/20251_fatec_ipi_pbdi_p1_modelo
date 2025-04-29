e1e5aeb53665935b234793de1bdb13d0ba2fb3bb
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-04-29 11:30:13 -0300

    feat(p1): encontra aprovados que estudam sozinhos

--- script.sql
+++ script.sql
@@ -43,7 +43,39 @@ CREATE TABLE alunos(
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
 --escreva a sua solução aqui
+-- DO $$
+-- DECLARE
+--     cur_alunos REFCURSOR;
+--     STUDANTID VARCHAR;
+--     GRADE INT;
+--     PREP_STUDY INT;
+--     cont INT := 0;
+-- BEGIN
+--     OPEN cur_alunos FOR 
+--     EXECUTE 
+--     format(
+--     'SELECT
+--         STUDANTID,
+--         GRADE,
+--         PREP_STUDY
+--         FROM alunos 
+--         WHERE PREP_STUDY = %s', 1);
 
+--     LOOP
+--         FETCH cur_alunos INTO STUDANTID, GRADE, PREP_STUDY;
+--         EXIT WHEN NOT FOUND;
+
+--         IF GRADE IS NOT NULL THEN
+--             RAISE NOTICE '% foi aprovado', STUDANTID;
+--             cont := cont + 1;
+--         ELSIF GRADE IS NULL THEN
+--             RAISE NOTICE 'O valor é nulo, então -1';
+--         END IF;
+--     END LOOP;
+
+--     CLOSE cur_alunos;
+-- END;
+-- $$;
 
 -- ----------------------------------------------------------------
 -- 4 Salário versus estudos
